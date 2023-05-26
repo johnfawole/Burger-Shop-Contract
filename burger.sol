5de08d5db2871b4d481bbbddb3c0f31845ef59bc
blessingbblockchain <adeolasola01@gmail.coml>
2023-05-26 11:34:25 +0100

    "Fixed errors and improve code in Burger contract"

--- burger.sol
+++ burger.sol
@@ -31,10 +31,10 @@
       uint orderseq;
       uint invoiceseq;
 
-      constructor(address _buyerAddress) public payable{
-          owner == msg.sender;
-
+      constructor(address payable _buyerAddress) public payable{
+          owner = payable(msg.sender);
           customer = _buyerAddress;
+        
       }
 
       function sendOrder(string memory burgerMenu, uint quantity) public payable{
@@ -55,7 +55,6 @@
       function sendPrice(uint orderNo, uint price) public payable{
           require(owner == msg.sender, "only the owner can do this, bruh");
           require(orders[orderNo].created, "The specific order does not exist");
-
           orders[orderNo].price = price;
       }
 
@@ -69,32 +68,24 @@
       function sendInvoice(uint orderNo, uint order_date) public payable {
           require(owner == msg.sender, "only the owner can do this, bruh");
           require(orders[orderNo].created, "The specific order does not exist");
-
           invoiceseq++;
-
           invoices[invoiceseq] = Invoice(invoiceseq, orderNo, true);
-
           orders[orderNo].orderDate = order_date;
       }
 
       function getInvoice(uint invoiceID) public view returns (address customer, uint orderNo, uint invoice_date) {
           require(invoices[invoiceID].created, "The invoice doesn't exist");
-
           Invoice storage _invoice = invoices[invoiceID];
           Order storage _order = orders[_invoice.orderNo];
-
            return(customer, _order.ID, _order.orderDate);
       }
 
       function markOrderDelivered(uint invoiceID, uint deliveryDate) public payable{
            require(customer == msg.sender, "Only customers can mark orders as delivered");
            require(invoices[invoiceID].created, "The invoice doesn't exist");   
-
            Invoice storage _invoice = invoices[invoiceID];
            Order storage _order = orders[_invoice.orderNo];
-
            _order.deliveryDate = deliveryDate;
-
            owner.transfer(_order.safePayment);
 
       }
